f98d8b1ef5451740753e716d01b7bd99a90463b9
Marc Newman <mnewman@gmail.com>
2024-11-24 05:35:06 -0500

    Remove unused Route53 zone data source
    The `aws_route53_zone` data source is no longer required and has been removed from the Terraform configuration. This change simplifies the file and removes unnecessary dependencies.

--- terraform/domain.tf
+++ terraform/domain.tf
@@ -1,7 +1,3 @@
-data "aws_route53_zone" "domain_zone" {
-  name = var.hosted_zone_name
-}
-
 resource "aws_amplify_domain_association" "domain_association" {
   app_id                = aws_amplify_app.marcnewman_me_amplify_app.id
   domain_name           = var.domain_name
