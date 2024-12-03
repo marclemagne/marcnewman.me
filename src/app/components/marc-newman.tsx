c3c25e2d8e347897aa3a6c8e05161aab4d5207e3
Marc Newman <mnewman@gmail.com>
2024-12-02 19:22:36 -0500

    Add clamp-based responsive font size to Tailwind config
    Introduce a new `clamp` property within Tailwind's `fontSize` configuration to support responsive text scaling. Updated the `MarcNewman` component to utilize the `text-clamp` class, ensuring consistent font size adjustments across different screen sizes. This enhancement improves design flexibility and visual consistency.

--- src/app/components/marc-newman.tsx
+++ src/app/components/marc-newman.tsx
@@ -1,6 +1,6 @@
 export default function MarcNewman() {
   return (
-    <h1 className="xs:text-6xl bg-gradient-to-r from-pink-500 via-purple-500 to-cyan-500 bg-clip-text text-center font-[family-name:var(--font-rubik-moonrocks)] text-4xl font-bold text-transparent md:text-8xl xl:text-9xl">
+    <h1 className="text-clamp bg-gradient-to-r from-pink-500 via-purple-500 to-cyan-500 bg-clip-text text-center font-[family-name:var(--font-rubik-moonrocks)] font-bold text-transparent">
       Marc Newman
     </h1>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontSize: {
+        // Tailwind does not have support for the `clamp` function
+        clamp: "clamp(2.25rem, 10vw, 8rem)", // text-4xl -> text-9xl
+      },
       screens: {
         xs: "320px",
         ...defaultTheme.screens,
